41bdb4f9ead7d44926f57a0d144994f26197dd06
OoGod <1617605689@qq.com>
2021-12-31 17:09:35 +0800

    update tools

---
+++ tools/k8s/cp_k8s.sh
@@ -0,0 +1,7 @@
+cp -r /tmp/k8s/cri-containerd.service /etc/systemd/system/cri-containerd.service
+cp -r /tmp/k8s/config.toml /etc/cri-containerd/config.toml
+cp -r /tmp/k8s/99-kubernetes-cri.conf /etc/sysctl.d/99-kubernetes-cri.conf
+cp -r /tmp/k8s/10-proxy-ipvs.conf /etc/systemd/system/kubelet.service.d/10-proxy-ipvs.conf
+sudo yum install -y kubelet kubeadm kubectl
+sysctl --system
+sudo systemctl daemon-reload
---
+++ tools/k8s/cp_k8s_to_remote.sh
@@ -0,0 +1,12 @@
+ip=${1}
+cd /tmp/k8s/
+sshpass -p password ssh root@${ip} mkdir /tmp/k8s/
+scp -r cri-containerd.service root@${ip}:/tmp/k8s/
+scp -r config.toml root@${ip}:/tmp/k8s/
+scp -r kubernetes.repo root@${ip}:/tmp/k8s/
+scp -r 99-kubernetes-cri.conf root@${ip}:/tmp/k8s/
+scp -r 10-proxy-ipvs.conf root@${ip}:/tmp/k8s/
+scp -r cp_k8s.sh root@${ip}:/root/
+scp -r cp_k8s.sh root@${ip}:/root/
+scp -r cp_k8s_to_remote.sh root@${ip}:/tmp/k8s/
+scp -r cp_k8s_to_remote.sh root@${ip}:/tmp/k8s/
